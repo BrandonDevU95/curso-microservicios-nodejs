2f8d984fec93b103e12ac952053dfc1c5018db0d
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-15 16:01:04 -0600

    chore: Update API route for user endpoint

--- api/index.js
+++ api/index.js
@@ -6,8 +6,8 @@ const user = require('./components/user/network');
 const app = express();
 
 //ROUTER
-app.use('/api/user', user);
 app.use(bodyParser.json());
+app.use('/api/user', user);
 
 app.listen(config.api.port, () => {
 	console.log(`Server running on http://localhost:${config.api.port}`);
