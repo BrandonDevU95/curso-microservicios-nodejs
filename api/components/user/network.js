6156e784e71ada7b80007b81653f853818ea563b
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-14 21:01:56 -0600

    chore: Update user list API endpoint to use controller and handle errors

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const response = require('../../../network/response');
 const router = express.Router();
-const Controller = require('./controller');
+const Controller = require('./index');
 
 router.get('/', function (req, res) {
-	const list = Controller.list();
-	response.success(req, res, list, 200);
+	Controller.list()
+		.then((list) => {
+			response.success(req, res, list, 200);
+		})
+		.catch((err) => {
+			response.error(req, res, err.message, 500);
+		});
+});
+
+router.get('/:id', function (req, res) {
+	const { id } = req.params;
+	Controller.get(parseInt(id))
+		.then((user) => {
+			response.success(req, res, user, 200);
+		})
+		.catch((err) => {
+			response.error(req, res, err.message, 500);
+		});
 });
 
 module.exports = router;
