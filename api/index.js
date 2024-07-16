b66ce2c1e235407a2f4062587c4ae61e543864ea
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-16 14:32:56 -0600

    chore: Add auth route to API

--- api/index.js
+++ api/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const config = require('../config');
 const bodyParser = require('body-parser');
 const user = require('./components/user/network');
+const auth = require('./components/auth/network');
 
 const app = express();
 
 //ROUTER
 app.use(bodyParser.json());
+
 app.use('/api/user', user);
+app.use('/api/auth', auth);
 
 app.listen(config.api.port, () => {
 	console.log(`Server running on http://localhost:${config.api.port}`);
