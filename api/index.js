a83b968499c91750fe22fffc9100b5713b56353c
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-17 06:02:00 -0600

    chore: Add error handling middleware for network errors

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const config = require('../config');
 const bodyParser = require('body-parser');
 const user = require('./components/user/network');
 const auth = require('./components/auth/network');
+const errors = require('../network/errors');
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(bodyParser.json());
 
 app.use('/api/user', user);
 app.use('/api/auth', auth);
+
+app.use(errors);
 
 app.listen(config.api.port, () => {
 	console.log(`Server running on http://localhost:${config.api.port}`);
