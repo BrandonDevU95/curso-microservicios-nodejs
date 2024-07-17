43ce15980c3a10fb929368253e8c86772050498f
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-17 15:52:26 -0600

    chore: Add following route to user controller

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -8,6 +8,7 @@ const router = express.Router();
 //Routes
 router.get('/', list);
 router.get('/:id', get);
+router.get('/:id/following', following);
 router.post('/', upsert);
 router.post('/follow/:id', secure('follow'), follow);
 router.put('/', secure('update'), upsert);
@@ -51,6 +52,14 @@ function follow(req, res, next) {
 	Controller.follow(req.user.id, req.params.id)
 		.then((data) => {
 			response.success(req, res, data, 201);
+		})
+		.catch(next);
+}
+
+function following(req, res, next) {
+	return Controller.following(req.params.id)
+		.then((data) => {
+			response.success(req, res, data, 200);
 		})
 		.catch(next);
 }
