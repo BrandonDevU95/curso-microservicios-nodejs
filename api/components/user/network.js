e746f8e58044948d7ef69ee4cb4d105d1c61ac9e
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-17 05:55:00 -0600

    chore: Update user secure middleware to use req.body.id for owner check

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const secure = require('./secure');
 const response = require('../../../network/response');
 const Controller = require('./index');
 
@@ -8,7 +9,7 @@ const router = express.Router();
 router.get('/', list);
 router.get('/:id', get);
 router.post('/', upsert);
-router.put('/', upsert);
+router.put('/', secure('update'), upsert);
 
 //Internal functions
 function list(req, res) {
