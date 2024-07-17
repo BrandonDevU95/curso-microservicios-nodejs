7a66fc3967e0d729cb9967450ee4a6fe920d7a5d
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-17 07:00:22 -0600

    chore: Update config.js to include MySQL connection details

--- config.js
+++ config.js
@@ -5,4 +5,10 @@ module.exports = {
 	jwt: {
 		secret: process.env.JWT_SECRET || 'notasecret!',
 	},
+	mysql: {
+		host: process.env.MYSQL_HOST || '172.18.0.2:3306',
+		user: process.env.MYSQL_USER || 'brandondev',
+		password: process.env.MYSQL_PASS || 'admin123',
+		database: process.env.MYSQL_DB || 'microservicios',
+	},
 };
