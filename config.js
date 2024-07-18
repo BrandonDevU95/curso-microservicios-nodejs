4e40d8795081d3b6d3cf66e5530553b1aac0440f
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-18 04:39:18 -0600

    chore: Update MySQL service port in config.js

--- config.js
+++ config.js
@@ -11,4 +11,7 @@ module.exports = {
 		password: process.env.MYSQL_PASS || 'admin123',
 		database: process.env.MYSQL_DB || 'microservicios',
 	},
+	mysqlService: {
+		port: process.env.MYSQL_SRV_PORT || 3001,
+	},
 };
