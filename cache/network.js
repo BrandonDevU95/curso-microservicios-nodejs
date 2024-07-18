f82efe3174fdfb6c26b3aa4dff6d5624d4601c8e
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-18 07:48:33 -0600

    chore: Add network routes for listing, getting, and upserting data

---
+++ cache/network.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const response = require('../network/response');
+const Store = require('../store/redis');
+
+const router = express.Router();
+
+router.get('/:table', list);
+router.get('/:table/:id', get);
+router.put('/:table', upsert);
+
+async function list(req, res, next) {
+	await Store.list(req.params.table)
+		.then((list) => {
+			response.success(req, res, list, 200);
+		})
+		.catch(next);
+}
+
+async function get(req, res, next) {
+	await Store.get(req.params.table, req.params.id)
+		.then((item) => {
+			response.success(req, res, item, 200);
+		})
+		.catch(next);
+}
+
+async function upsert(req, res, next) {
+	await Store.upsert(req.params.table, req.body)
+		.then((item) => {
+			response.success(req, res, item, 200);
+		})
+		.catch(next);
+}
+
+module.exports = router;
