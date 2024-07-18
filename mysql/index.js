169c5612e14434d304a811fd894b8d559dfbac13
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-18 04:39:26 -0600

    chore: Add MySQL service initialization and API routes for posts

---
+++ mysql/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const config = require('../config');
+const router = require('./network');
+const app = express();
+
+app.use(bodyParser.json());
+
+//Routes
+app.use('/', router);
+
+app.listen(config.mysqlService.port, () => {
+	console.log(`MySQL Service started on port ${config.mysqlService.port}`);
+});
