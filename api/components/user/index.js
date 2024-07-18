bb882310924bb523b7afd6da4854a46e5780d4e3
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-18 08:03:38 -0600

    chore: Update user module to include Redis cache and store modules

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,11 +1,14 @@
 const config = require('../../../config');
 
 let store;
+let cache;
 
 if (config.remoteDB) {
 	store = require('../../../store/remote-mysql');
+	cache = require('../../../store/remote-cache');
 } else {
 	store = require('../../../store/mysql');
+	cache = require('../../../store/redis');
 }
 const controller = require('./controller');
 
